7cf87e0f0d9d3774115bdf82ee5c5d7c0310221e
rubensancor <rubensancor@103L9.opendeusto.es>
2014-10-13 12:49:27 +0200

    Pre-Alpha Numi

--- Topetes/src/ventanas/VentanaPrincipal.java
+++ Topetes/src/ventanas/VentanaPrincipal.java
@@ -15,10 +15,14 @@ import java.util.Random;
 
 public class VentanaPrincipal extends JFrame{
 
+
 	static JPanel arrayPaneles [][]= new JPanel [4][3];
-	ArrayList <JLabel> arrayDeTopos = new ArrayList<>();
+	static ArrayList <JLabel> arrayDeTopos = new ArrayList<>();
+	MiRunnable miHilo = null;  // Hilo principal del juego
+
 	public VentanaPrincipal() {
-	
+
+		
 		arrayDeTopos = new ArrayList<>();
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.setColumns(3);
@@ -26,7 +30,7 @@ public class VentanaPrincipal extends JFrame{
 		setLayout(gridLayout);
 		setVisible(true);
 		setSize(new Dimension(400,600));
-		
+
 		for(int i=0; i<gridLayout.getRows(); i++)
 		{
 			for(int j=0 ; j<gridLayout.getColumns(); j++)
@@ -58,36 +62,73 @@ public class VentanaPrincipal extends JFrame{
 					@Override
 					public void mouseReleased(MouseEvent e) {
 						try{
-						System.out.println("ENTRA EN EL PANEL"+k+l);
-						arrayPaneles[k][l].remove(arrayPaneles[k][l].getComponent(0));
-						arrayDeTopos.remove(0);
-						arrayPaneles[k][l].repaint();
+							System.out.println("ENTRA EN EL PANEL"+k+l);
+							arrayPaneles[k][l].remove(arrayPaneles[k][l].getComponent(0));
+							arrayDeTopos.remove(0);
+							arrayPaneles[k][l].repaint();
 						}catch(ArrayIndexOutOfBoundsException a)
 						{
 							System.out.println("NO HAY JLABEL");
 						} catch(IndexOutOfBoundsException e12){
-							
+
 						}
-						}
-					
+					}
+
 				}
-				);
-				
+						);
+
 			}
 		}
 	}
 	
-	public void creaTopo ()
+
+	public static void creaTopo ()
 	{
 		JLabel labelTopo = new JLabel("TOPO");
 		arrayDeTopos.add(labelTopo);
 		Random r = new Random();
 		arrayPaneles[r.nextInt(4)][r.nextInt(3)].add(labelTopo);
+		System.out.println("-->" + r.nextInt(4) +" "+ r.nextInt(3) + "<--" );
+		for (int i = 0; i < 4; i++) { 
+			for (int j = 0; j < 3; j++) {
+				
+				arrayPaneles[i][j].repaint();	//Repaint a los paneles para que añada otro topo
+				System.out.println("asdf");
+			}
+		}
+		
 	}
 	public static void main(String[] args) {
 		VentanaPrincipal ventana = new VentanaPrincipal();
-		ventana.creaTopo();
+		ventana.miHilo = new MiRunnable();  // Sintaxis de new para clase interna
+		Thread nuevoHilo = new Thread( ventana.miHilo );
+		nuevoHilo.start();
 	}
-	
-	
 }
+
+	
+	/**
+	 * @author rubensancor
+	 * Clase robada a anoni para el hilo :D
+	 */
+	class MiRunnable implements Runnable {
+		boolean sigo = true;
+
+		@Override
+		public void run() {
+			while (sigo) {
+			VentanaPrincipal.creaTopo();
+			
+			try {
+				Thread.sleep( 5200 );
+			} catch (Exception e) {
+			}
+			}
+		}
+		/** Ordena al hilo detenerse en cuanto sea posible
+		 */
+		public void acaba() {
+			sigo = false;
+		}
+
+	}
