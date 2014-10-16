0b4514a81a03725897eca9eb676d2bf340241735
Rubén Sánchez <rubensancor@opendeusto.es>
2014-10-16 19:48:33 +0200

    Numi trabajando en VentanaPrincipal Pre-Alpha

--- Topetes/src/ventanas/VentanaPrincipal.java
+++ Topetes/src/ventanas/VentanaPrincipal.java
@@ -12,6 +12,7 @@ import java.awt.Panel;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 
@@ -20,10 +21,11 @@ public class VentanaPrincipal extends JFrame{
 
 	private static final long serialVersionUID = 1096374592905539346L;
 
-	static JPanel arrayPaneles [][] = new JPanel [4][3];
+	static JPanel arrayPaneles [][] = new JPanel [4][3]; // Array de paneles que contienen los jlabel de los topos
 	static int arrayAlternativo [][] = new int [4][3];	//Array alternativo con el numero de veces clicadas en ese panel
-	static ArrayList <Topete> arrayDeTopos = new ArrayList<>();
+	static Topete arrayTopos[][] = new Topete[4][3]; //Array que va a contener los topos
 	MiRunnable miHilo = null;  // Hilo principal del juego
+	public static int puntuacion;
 
 	public VentanaPrincipal() {
 		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
@@ -34,7 +36,6 @@ public class VentanaPrincipal extends JFrame{
 			}
 
 		}
-		arrayDeTopos = new ArrayList<>();
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.setColumns(3);
 		gridLayout.setRows(4);
@@ -69,17 +70,24 @@ public class VentanaPrincipal extends JFrame{
 					}
 
 					@Override
-					public void mouseReleased(MouseEvent e) {	// TODO Hay que saber a que topo golpea para saber cuantas vidas tiene y matarlo
+					public void mouseReleased(MouseEvent e) {
 						try{
-							if(e.getComponent() instanceof JLabelTopete){
-								Topete temp = arrayPaneles[k][l].getComponent(0);							
-								temp.pegaTopo();
-								if(temp.getVidas()<=0){
-									arrayPaneles[k][l].remove(arrayPaneles[k][l].getComponent(0));
-									arrayDeTopos.remove(0);
-									arrayPaneles[k][l].repaint();
-									arrayAlternativo[k][l] = 0;
+							//							if(e.getComponent() instanceof JLabelTopete){
+							JLabel temp = (JLabel) e.getComponent();
+							for (int i = 0; i<arrayPaneles.length; i++) {
+								for (int j= 0; j<arrayPaneles[i].length; j++) {
+									if(arrayPaneles[i][j].equals(temp))
+										arrayTopos[i][j].pegaTopo();
+
+									System.out.println(arrayTopos[i][j].getVidas());
+									//									if(topete.getVidas()<=0){
+									//										arrayPaneles[k][l].remove(arrayPaneles[k][l].getComponent(0));
+									//										arrayDeTopos.remove(0);
+									//										arrayPaneles[k][l].repaint();
+									//										arrayAlternativo[k][l] = 0;
+									//									}
 								}
+
 							}
 						}catch(ArrayIndexOutOfBoundsException a)
 						{
@@ -101,19 +109,24 @@ public class VentanaPrincipal extends JFrame{
 	{
 		Topete topo;
 		do{
-					
+
 			topo = new Topete(TipoTopo.NORMAL);
-			
+
 		}while(arrayPaneles[topo.getPosX()][topo.getPosY()].getComponents().length==1);  //Evita que si ya hay un topo en el espacio seleccionado, se cree otro 
-		arrayDeTopos.add(topo);
+		arrayTopos[topo.getPosX()][topo.getPosY()]=topo;
 		arrayPaneles[topo.getPosX()][topo.getPosY()].add(topo.getImg());
 		arrayPaneles[topo.getPosX()][topo.getPosY()].validate();
 		arrayPaneles[topo.getPosX()][topo.getPosY()].repaint();	//Repaint a los paneles para que añada otro topo
-		System.out.println(arrayDeTopos.size());
-		
-
-
 	}
+
+	public static boolean estamosLLenos(){
+		for (int i = 0; i < arrayTopos.length; i++) {
+			for (int j = 0; j < arrayTopos[i].length; j++) {
+				if(ar[i][j])
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		VentanaPrincipal ventana = new VentanaPrincipal();
 		ventana.miHilo = new MiRunnable();  // Sintaxis de new para clase interna
@@ -135,9 +148,9 @@ class MiRunnable implements Runnable {
 	public void run() {
 
 		while (sigo) {
-			
-			if(VentanaPrincipal.arrayDeTopos.size()<12)
-//			VentanaPrincipal.creaTopo();
+
+			if(VentanaPrincipal.arrayTopos.size()<12)
+				VentanaPrincipal.creaTopo();
 
 			try {
 				Thread.sleep( 1000 );
