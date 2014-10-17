62974c47b308cf197068716c06e9906140bb6f4a
Gaizka Virumbrales <gvirum@gmail.com>
2014-10-17 12:42:46 +0200

    Alpha images

--- Topetes/src/animales/topos/jlabels/JLabelTopete.java
+++ Topetes/src/animales/topos/jlabels/JLabelTopete.java
@@ -14,16 +14,18 @@ import animales.topos.TipoTopo;
 public class JLabelTopete extends JLabel {
 
 	private static final long serialVersionUID = 741001305416508022L;
+	protected TipoTopo tipo;
 	int id;
 	
 	public JLabelTopete( TipoTopo t ) { 
+		tipo = t;
 		try {
 			switch (t) {
 			case NORMAL:
-				this.setIcon(new ImageIcon("src/img/topeteNormal.png"));
+				this.setIcon(new ImageIcon("src/img/topeteNormalR.png"));
 				break;
 			case MASAO:
-				this.setIcon(new ImageIcon("src/img/topeteMasao.png"));
+				this.setIcon(new ImageIcon("src/img/topeteMasaoR.png"));
 				break;
 			default:
 				break;
@@ -36,20 +38,31 @@ public class JLabelTopete extends JLabel {
 		setBounds( 0, 0, 10, 30);		
 	}
 	
-//	 Redefinición del paintComponent para que se escale el gráfico
-	@Override
-	protected void paintComponent(Graphics g) {
-//		super.paintComponent(g);   // En este caso no nos sirve el pintado normal de un JLabel
-		Image img = ((ImageIcon)getIcon()).getImage();
-		Graphics2D g2 = (Graphics2D) g;  // El Graphics realmente es Graphics2D
-		// Escalado más fino con estos 3 parámetros:
-		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-		g2.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);	
-		
-
-        g2.drawImage( img, 0, 0, 10, 30, null );
-	}
+////	 Redefinición del paintComponent para que se escale el gráfico
+//	@Override
+//	protected void paintComponent(Graphics g) {
+////		super.paintComponent(g);   // En este caso no nos sirve el pintado normal de un JLabel
+//		Image img;
+//		switch (tipo) {
+//		case NORMAL:
+//			img = new ImageIcon("src/img/topeteNormal.png").getImage();
+//			break;
+//		case MASAO:
+//			img = new ImageIcon("src/img/topeteMasao.png").getImage();
+//			break;
+//		default:
+//			img = null;
+//			break;
+//		}
+//		Graphics2D g2 = (Graphics2D) g;  // El Graphics realmente es Graphics2D
+//		// Escalado más fino con estos 3 parámetros:
+//		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+//		g2.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+//		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);	
+//		
+//        g2.drawImage( img, 0, 0, 10, 30, null );
+//
+//	}
 	// TODO: movimiento de salir
 
 }
