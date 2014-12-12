103fdbeee6562467a9f7bd0ffbb53ca2ba9c7178
gorkabarturen <gorka.barturen@opendeusto.es>
2014-12-12 12:22:44 +0100

    asd

--- Topetes/src/ventanas/VentanaPrincipalDos.java
+++ Topetes/src/ventanas/VentanaPrincipalDos.java
@@ -1,127 +1,118 @@
 package ventanas;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
 
+import animales.topos.Topete;
+import animales.topos.TipoTopo;
 
-public class VentanaPrincipalDos{
-	  static JFrame miVentana;
-	    static JPanel p1;
-	    static JPanel p2;
-	    static JPanel p3;
-	    static JPanel p4;
-	    static JPanel p5;
-	    static JPanel p6;
-	    static JPanel p7;
-	public VentanaPrincipalDos ()
-	{
-//		JFrame frame = new JFrame();
-//		
-// 		JLayeredPane lp = new JLayeredPane();
-//
-// 		JPanel panel1 = new JPanel();
-//			JLabel imagen1 = new JLabel(new ImageIcon("src/img/panel0.png"));
-//			panel1.add(imagen1);
-//		JPanel panel2 = new JPanel();
-//			JLabel imagen2 = new JLabel(new ImageIcon("src/img/panel1.png"));
-//			panel2.add(imagen2);
-//		JPanel panel3 = new JPanel();
-//			JLabel imagen3 = new JLabel(new ImageIcon("src/img/panel2.png"));
-//			panel3.add(imagen3);
-//		JPanel panel4 = new JPanel();
-//			JLabel imagen4 = new JLabel(new ImageIcon("src/img/panel3.png"));
-//			panel4.add(imagen4);
-//		JPanel panel5 = new JPanel();
-//			JLabel imagen5 = new JLabel(new ImageIcon("src/img/panel4.png"));
-//			panel5.add(imagen5);
-//
-//			frame.setLayeredPane(lp);
-//			
-//			lp.add(panel5, new Integer(4));
-//			lp.add(panel4, new Integer(3));
-//			lp.add(panel3, new Integer(2));
-//			lp.add(panel2, new Integer(1));
-//			lp.add(panel1, new Integer(0));
-//			
-//			frame.setVisible(true);
-		
-		miVentana = new JFrame();
-		 p1 = new JPanel();
-     	p1.setName( "p1" );
-			p1.setBorder( BorderFactory.createMatteBorder( 2, 5, 2, 5, Color.green ) );
-			p1.setBounds( 10, 10, 400, 150 );
-     p2 = new JPanel();
-     	p2.setName( "p2" );
- 		Border b2 = BorderFactory.createBevelBorder( BevelBorder.RAISED );
- 		p2.setBorder( b2 );
-			p2.setBounds( 30, 30, 400, 150 );
-     p3 = new JPanel();
-     	p3.setName( "p3" );
- 		Border b3 = BorderFactory.createEtchedBorder();
- 		p3.setBorder( b3 );
-			p3.setBounds( 50, 50, 400, 150 );
+public class VentanaPrincipalDos {
+	
+    static JFrame miVentana;
+    static JPanel p1;
+    static JPanel p2;
+    static JPanel p3;
+    static JPanel p4;
+    static JPanel p5;
+    static JPanel p6;
+    static JPanel p7;
+    
+    static JPanel panelImagen1;
+    static JPanel panelImagen2;
+    static JPanel panelImagen3;
+    static JPanel panelImagen4;
+    
+    static Topete normal1 = new Topete(TipoTopo.CASCO);
+    static Topete normal2 = new Topete(TipoTopo.CASCO);
+    static Topete normal3 = new Topete(TipoTopo.JUGGERNAUT);
+    static Topete normal4 = new Topete(TipoTopo.CASCO);
+    static Topete normal5 = new Topete(TipoTopo.CASCO);
+    static Topete normal6 = new Topete(TipoTopo.CASCO);
+    static Topete normal7 = new Topete(TipoTopo.CASCO);
+    static Topete normal8 = new Topete(TipoTopo.CASCO);
+    static Topete normal9 = new Topete(TipoTopo.NORMAL);
+    
+    
+    public VentanaPrincipalDos () {
+    	
+    	// Crear ventana inicial
+        miVentana = new JFrame("Prueba de paneles de Swing");        
+        // Acabar de crear y hacer visible ventana
+        miVentana.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+        miVentana.setSize( 740, 760 );
+        
+        // Creación de los fondos 
+        p1 = new JPanel();
+			JLabel imagen1 = new JLabel( new ImageIcon("src/img/panel0.png"));
+        	p1.setBounds(0, 0, miVentana.getHeight(), miVentana.getWidth());
+        	p1.add(imagen1);
+//        	p1.setBorder(BorderFactory.createLineBorder(Color.RED));
+        	p1.setOpaque(false);
+        	
+        p2 = new JPanel();
+			JLabel imagen2 = new JLabel( new ImageIcon("src/img/panel1.png"));
+    		p2.setBounds(0, 165, 740, 595);
+    		p2.add(imagen2);
+//			p2.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        	p2.setOpaque(false);
+
+        p3 = new JPanel();
+			JLabel imagen3 = new JLabel( new ImageIcon("src/img/panel2.png"));
+    		p3.setBounds(0, 340, 740, 688);
+    		p3.add(imagen3);
+//			p3.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        	p3.setOpaque(false);
+
 		p4 = new JPanel();
-			p4.setName( "p4" );
- 		p4.setBorder( BorderFactory.createLineBorder( Color.red ) );
-			p4.setBounds( 70, 70, 400, 150 );
+			JLabel imagen4 = new JLabel( new ImageIcon("src/img/panel3.png"));
+    		p4.setBounds(0, 530, 740, 760);
+    		p4.add(imagen4);
+//			p4.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
+        	p4.setOpaque(false);
+
 		p5 = new JPanel();
-			p5.setName( "p5" );
- 		p5.setBorder( BorderFactory.createLoweredBevelBorder() );
-			p5.setBounds( 90, 90, 400, 150 );
-		p6 = new JPanel();
- 		p6.setName( "p6" );
- 		p6.setBorder( BorderFactory.createRaisedBevelBorder() );
-			p6.setBounds( 110, 110, 400, 150 );
-		p7 = new JPanel();
-			p7.setName( "p7" );
- 		Border b7 = BorderFactory.createCompoundBorder(
-                 BorderFactory.createTitledBorder("Nombre de panel"),
-                 BorderFactory.createEmptyBorder(5,5,5,5));
-     		p7.setBorder( b7 );
- 		p7.setBounds( 130, 130, 400, 150 );
- 	miVentana.getContentPane().setName( "princ" );
- 			
-		p1.add( new JLabel("[CRISTAL] Ejemplo de panel con borde matte (verde)") );
-		p1.setOpaque( false );
-		p2.add( new JLabel("[2] Ejemplo de panel con borde Bevel") );
-		p3.add( new JLabel("[3] Ejemplo de panel con borde etched") );
-		p4.add( new JLabel("[4] Ejemplo de panel con borde line (rojo)") );
-		p5.add( new JLabel("[5] Ejemplo de panel con borde lowered") );
-		p6.add( new JLabel("[6] Ejemplo de panel con borde raised") );
-		p7.add( new JLabel(new ImageIcon("src/img/panel0.png")));
+			JLabel imagen5 = new JLabel( new ImageIcon("src/img/panel4.png"));
+	    	p5.setBounds(0, 705, 740, 760);
+	    	p5.add(imagen5);
+//			p5.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+        	p5.setOpaque(false);
+
+        	// Creación de los topos
+        	
+        	panelImagen1 = new JPanel();
+        		panelImagen1.setBounds(0, 0, 740, 760);
+
+        		panelImagen1.add(normal1.getImg());
+//        		panelImagen1.add(normal3.getImg());
+//        		panelImagen1.add(normal9.getImg());
+        	
 
 		JLayeredPane lp = new JLayeredPane();
 		
 		miVentana.setLayeredPane( lp );
-		p7.setOpaque( false );
-     			
-		lp.add( p7, new Integer(7) );
-		lp.add( p6, new Integer(6) );
-		lp.add( p5, new Integer(5) );
-		lp.add( p4, new Integer(-4) );
-		lp.add( p3, new Integer(3) );
-		lp.add( p2, new Integer(2) );
-
-		JPanel cristal = new JPanel();
-		cristal.setLayout( null );
-		miVentana.setGlassPane( cristal );
-		cristal.add( p1 );
-		cristal.setOpaque( false );
-		cristal.setVisible( true );
+        			
+//		lp.add( p5, new Integer(40) );
+//		lp.add( p4, new Integer(30) );
+//		lp.add( p3, new Integer(20) );
+//		lp.add( p2, new Integer(10) );
+//		lp.add(panelImagen1, new Integer(5));
+		lp.add( p1, new Integer(0) );
 		
-     miVentana.setSize( 600, 500 );
-     miVentana.setLocationRelativeTo(null);  // Centrar en pantalla
-     miVentana.setVisible(true);
-	}
-	
 
+        miVentana.setLocationRelativeTo(null);  // Centrar en pantalla
+        miVentana.setVisible(true);
+        
+       
+    }
 	
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		new VentanaPrincipalDos();
 	}
-	
 	
 }
